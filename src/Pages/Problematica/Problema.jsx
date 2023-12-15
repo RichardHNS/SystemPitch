da580943768a3f5edfa07b50dfb1edcc6f241d6a
Richard Henrique <richard.estudo08@gmail.com>
2023-12-15 14:16:19 -0300

    ajustando

--- src/Pages/Problematica/Problema.jsx
+++ src/Pages/Problematica/Problema.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import NavBar from "../../Components/NavBar/NavBar";
 import "./Problematica.css";
 import Acidente from "/src/assets/Acidente.jpg";
-import Reclamacao from "/src/assets/Reclamacao.png"
-import Desafios from "/src/assets/Desafios.png"
-import Eficacia from "/src/assets/Eficacia.png"
+import Reclamacao from "/src/assets/Reclamacao.png";
+import Desafios from "/src/assets/Desafios.png";
+import Eficacia from "/src/assets/Eficacia.png";
 const Problema = () => {
   return (
     <>
@@ -14,14 +14,21 @@ const Problema = () => {
         <div className="Acidente">
           <ul className="ContentUl">
             <li>
-               Falta de controle efetivo por parte das empresas do uso de equipamentos de proteção individual.
+              Falta de controle efetivo por parte das empresas do uso de
+              equipamentos de proteção individual.
             </li>
             <li>
               Ausência de uma cultura de segurança eficiente nas micro e
               pequenas empresas.
             </li>
           </ul>
-          <img className="imgAcidente" src={Acidente} alt="" width="300px" height="200px" />
+          <img
+            className="imgAcidente"
+            src={Acidente}
+            alt=""
+            width="300px"
+            height="200px"
+          />
           <div className="Dados">
             <p>
               De 2012 a 2020, de acordo com dados do Observatório de Segurança e
@@ -43,24 +50,27 @@ const Problema = () => {
             </ul>
           </div>
           <div className="PesqValida">
-              <p className="PTitle">Formulário do problema</p>
-              <p className="PForm">Para a validação do problema, entramos em contato com 12 profissionais de SST. Abaixo segue os dados apurados:</p>
-              <div className="Q1">
-                <h5>Reclamações mais frequentes ouvidas pelos gestores:</h5>
-                <img src={Reclamacao} alt="" width="300px" />
-              </div>
-              <div className="Q2">
-              <h5>Maiores desafios na área de Segurança do Trabalho:</h5>
-                <img src={Desafios} alt="" width="300px" />
-              </div>
-              <div className="Q3">
-              <h5>Eficácia de um controle de Epis na prevenção de Acidentes:</h5>
-                <img src={Eficacia} alt="" width="300px" />
-              </div>
-              <div>
-                
-              </div>
+            <p className="PTitle">Formulário do problema</p>
+            <p className="PForm">
+              Para a validação do problema, entramos em contato com 12
+              profissionais de SST. Abaixo segue os dados apurados:
+            </p>
+            <div className="Q1">
+              <h5>Reclamações mais frequentes ouvidas pelos gestores:</h5>
+              <img src={Reclamacao} alt="" width="300px" />
             </div>
+            <div className="Q2">
+              <h5>Maiores desafios na área de Segurança do Trabalho:</h5>
+              <img src={Desafios} alt="" width="300px" />
+            </div>
+            <div className="Q3">
+              <h5>
+                Eficácia de um controle de Epis na prevenção de Acidentes:
+              </h5>
+              <img src={Eficacia} alt="" width="300px" />
+            </div>
+            <div></div>
+          </div>
         </div>
       </div>
     </>
